7c1dd4f3627366a6d6bb9a55684012b945bfa6c7
jotkafomat <k-j@xl.wp.pl>
2021-09-17 13:37:55 +0100

    connect outlets

---
+++ TodayWithUIKit/ReminderListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderListCell.swift
+//  TodayWithUIKit
+//
+//  Created by Krzysztof Jankowski on 17/09/2021.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    
+    @IBOutlet var doneButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var titleLabel: UIView!
+}
