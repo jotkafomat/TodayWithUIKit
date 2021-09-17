afd61cbc9bedb554dbbb956ea48da1f0834da441
jotkafomat <k-j@xl.wp.pl>
2021-09-17 15:57:38 +0100

    connect done button

--- TodayWithUIKit/ReminderListCell.swift
+++ TodayWithUIKit/ReminderListCell.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class ReminderListCell: UITableViewCell {
     
+    typealias DoneButtonAction = () -> Void
+    
     @IBOutlet var doneButton: UIButton!
     @IBOutlet var dateLabel: UILabel!
-    @IBOutlet var titleLabel: UIView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    var doneButtonAction: DoneButtonAction?
+    
+    @IBAction func doneButtonTriggered(_ sender: UIButton) {
+           doneButtonAction?()
+       }
 }
