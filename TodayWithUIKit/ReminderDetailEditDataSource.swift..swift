7cdb36280654766a4c1bcaaafedcf33458503f44
jotkafomat <k-j@xl.wp.pl>
2021-10-31 12:07:57 +0000

    add data source

---
+++ TodayWithUIKit/ReminderDetailEditDataSource.swift..swift
@@ -0,0 +1,58 @@
+//
+//  ReminderDetailEditDataSource.swift..swift
+//  TodayWithUIKit
+//
+//  Created by Krzysztof Jankowski on 31/10/2021.
+//
+
+import UIKit
+
+class ReminderDetailEditDataSource: NSObject {
+    enum ReminderSection: Int, CaseIterable {
+        case title
+        case dueDate
+        case notes
+        
+        var displayText: String {
+            switch self {
+            case .title:
+                return "Title"
+            case .dueDate:
+                return "Date"
+            case .notes:
+                return "Notes"
+            }
+        }
+        
+        func cellIdentifier(for row: Int) -> String {
+            switch self {
+            case .title:
+                return "EditTitleCell"
+            case .dueDate:
+                return row == 0 ? "EditDateLabelCell" : "EditDateCell"
+            case .notes:
+                return "EditNotesCell"
+            }
+        }
+    }
+}
+
+extension ReminderDetailEditDataSource: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return ReminderSection.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        fatalError()
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+           guard let section = ReminderSection(rawValue: section) else {
+               fatalError("Section index out of range")
+           }
+           return section.displayText
+       }
+}
